22a168763a72fcac7074be1288cb29d89485109d
Hynek V. Svobodny <hynek.vaclav.svobodny@tul.cz>
2022-12-12 18:30:39 +0100

    Create interfaces.js

---
+++ interfaces.js
@@ -0,0 +1,34 @@
+// This file can not be run as it contains invalid syntax
+
+// main.js
+existingTiles = {A: Tile, B: Tile, C: Tile];
+
+
+// board.js
+
+class Tile {
+  allowedTileTypes = {
+    A: []
+    B: []
+    C: []
+    D: []
+  };
+  url = "/imgs/xx.png"
+}
+
+class Board {
+  width,
+  height,
+  tiles = [
+    [Tile, Tile, Tile],
+    [],
+  ];
+  addTile(x, y, type) {}
+  getAllowedTiles(x,y) {}
+  getURL(x,y) {}
+}
+
+
+// generator.js
+
+function generate(board, existingTiles)
