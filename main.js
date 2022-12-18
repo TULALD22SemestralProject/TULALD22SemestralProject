28a627fae0ca57e64742803fbfa953813540437b
hyneq <hyneqs@gmail.com>
2022-12-18 17:04:30 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Creates a new Board and calls generate() to fill it with tiles and then calls fillGrid that fills a HTML image grid acording to data from the Board  
 function generateWorld(grid, width, height, existingTiles) {
     let board = new Board(width, height);
     generate(board, existingTiles);
@@ -6,17 +7,17 @@ function generateWorld(grid, width, height, existingTiles) {
 
 // Fills a HTML grid element with tiles from a Board
 function fillGrid(grid, board) {
-    grid.innerHTML = ""; // clear the element
+    grid.innerHTML = ""; // clear the grid
 
     grid.style.display = "grid";
     grid.style.gridTemplateColumns = `repeat(${board.width}, auto)`;
     
     let image;
-    board.tiles.foreach((row) => {
-        row.foreach((tile) => {
+    for (let y=0; y < board.height; y++) {
+        for (let x=0; x < board.width; x++) {
             image = document.createElement("image");
-            image.src = tile.url;
+            image.src = board.getURL(x,y);
             grid.appendChild(image);
-        });
-    });
+        }
+    }
 }
