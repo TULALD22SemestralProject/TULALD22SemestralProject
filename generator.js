5be7c046624ba034a64a664499d168a3f2de6bee
Jakub Silhan <jakub.silhan@tul.cz>
2022-12-19 09:33:41 +0100

    Created first generator

---
+++ generator.js
@@ -0,0 +1,18 @@
+
+
+function generate(board, existingTiles){
+    for (let i = 0; i < board.height; i++) {
+        for (let j = 0; j < board.width; j++) {
+            const AllowedArray = board.getAllowedTiles(j,i);
+            //picks random tile from allowed
+            const randomTile = getRandomTile(AllowedArray);
+            //places random tile on the board
+            board.addTile(j,i, randomTile);
+        }        
+    }
+}
+
+function getRandomTile(tiles){
+    const randomTile = tiles[Math.floor(Math.random()*tiles.length)]
+    return randomTile
+}
